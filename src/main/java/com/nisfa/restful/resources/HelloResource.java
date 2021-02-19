90b34ba4bff06d2a034a3ea5f6a0e0ff3f5087c9
MohamedFaruk_M <msmohamedfaruk@gmail.com>
2021-02-19 13:33:26 +0530

    Added doc for Base Resource - version control

--- src/main/java/com/nisfa/restful/resources/HelloResource.java
+++ src/main/java/com/nisfa/restful/resources/HelloResource.java
@@ -1,5 +1,6 @@
 package com.nisfa.restful.resources;
 
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -8,6 +9,17 @@ import javax.ws.rs.core.Application;
  * @author Administrator
  */
 @ApplicationPath("api/v1")
-public class HelloResource extends Application{
-    
+public class HelloResource extends Application {
+
+    /**
+     * primarily used to polymorphic the BaseResource from the different version
+     * control
+     *
+     * @return
+     */
+//    @Override
+//    public Set<Class<?>> getClasses() {
+//        return super.getClasses();
+//
+//    }
 }
